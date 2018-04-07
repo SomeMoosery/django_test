dd7a2ee470456faf6a6f6c35f0bf513f04d03cfa
Carter <carterklein13@gmail.com>
2018-04-07 00:27:23 -0500

    Fixed comment bug, had to set default value (empty string) for comments otherwise model wouldn't migrate

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name="comments")
     author = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.TextField(default="")
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
